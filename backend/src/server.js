47e83c67f1cfa9b83324f35791cf4fa3421efa73
Amruth M S <msamruth74@gmail.com>
2025-09-06 01:22:49 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+const notesRoutes = require("./routes/notesRoutes.js");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(express.json());
+
+// Enable CORS so frontend (different port) can call backend
+app.use(
+  cors({
+    origin: ["http://localhost:5173", 
+      "https://note-keepify.vercel.app" , 
+      "https://keepify-frontend.onrender.com"], // your Vite frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+// Routes
+app.use("/api/notes", notesRoutes);
+
+// Server start
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
+});
